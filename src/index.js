90304f1d4ba3ed47678eab8499894985a6d7e389
Luiz Pedro <luizpsmoreira@gmail.com>
2020-01-17 20:57:48 -0300

    add websocket OmniStack 10 dia 5

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const http = require('http');
 const routes = require('./routes');
-require('dotenv').config()
+const { setupWebsocket } = require('./websocket');
+require('dotenv').config();
 
 const app = express();
+
+const server = http.Server(app);
+
+setupWebsocket(server);
 // MongoDB (Não-relacional)
 mongoose.connect(process.env.MONGO_DB_CONNECTION, {
     useNewUrlParser: true,
@@ -15,4 +21,4 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333);
+server.listen(3333);
